d297c7c7b01cdb3142ce282de85ea6118a31b18d
AlxXxlA <lxy18940186659@163.com>
2023-12-22 11:15:23 +0800

    x

---
+++ js/tongji.js
@@ -0,0 +1,7 @@
+﻿var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?f5efd19f59eafa8ad284ee8daa803c7e";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
